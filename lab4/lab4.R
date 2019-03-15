9f496a22527e4cbe593c8ebec1f02e4efe5504f9
mohamed ali <muhamedali166@gmail.com>
2019-03-16 00:54:18 +0200

    lab4 finished

--- lab4/lab4.R
+++ lab4/lab4.R
@@ -1,5 +1,8 @@
 # Clean environment 
+# Req1
 rm(list=ls())
+library("png")
+library("ggplot2")
 
 # Set working directory with source file's directory.
 dir <- dirname(parent.frame(2)$ofile)
@@ -45,3 +48,28 @@ plot(dfm, col=km2$cluster)
 # Plot centroids.
 points(km2$center,col=4:6,pch=24,cex=1.5, bg=4:6)
 
+
+# Req2
+img <- readPNG("bird_small.png")
+img_dm <- dim(img)
+img_dfm <- data.frame( R = as.vector(img[,,1]),
+                       G = as.vector(img[,,2]),
+                       B = as.vector(img[,,3]))
+
+# Perform clustering. 
+img_km <- kmeans(img_dfm,16,15)
+
+# plotting clusters.
+img_clusters<-as.vector(img_km$cluster)
+
+img_clusters_val <- img_km$centers[img_clusters,]
+# Centers display the needed RGB values only (16 value only)
+
+# Assign clusters to pixels.
+img_dfm$R <- img_clusters_val[,1]
+img_dfm$G <- img_clusters_val[,2]
+img_dfm$B <- img_clusters_val[,3]
+writeImage(img_dfm, "compressed.png")
+img_compressed <- data.matrix(img_dfm, rownames.force = NA)
+dim(img_compressed) <- c(128,128)
+writePNG(img_compressed, "compressed.png")
